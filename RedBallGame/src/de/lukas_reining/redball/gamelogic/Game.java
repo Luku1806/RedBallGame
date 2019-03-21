9564e35bfe110de37e951634aef5d867e5b28a83
Luku1806 <45849506+Luku1806@users.noreply.github.com>
2019-03-21 19:31:13 +0100

    Also reverted jumping

--- RedBallGame/src/de/lukas_reining/redball/gamelogic/Game.java
+++ RedBallGame/src/de/lukas_reining/redball/gamelogic/Game.java
@@ -49,7 +49,7 @@ public class Game extends GameLoop {
 		if (keys.right_pressed()) {
 			currentWorld.getPlayer().moveRight();
 		}
-		if (keys.space_typed()) {
+		if (keys.space_pressed()) {
 			currentWorld.getPlayer().jump();
 			System.out.println("Juuump");
 		}
